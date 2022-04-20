666922d5c2cddc7eebfba944d44fe35bc829232e
Jakob <jakob.priesner@outlook.de>
2022-04-20 19:36:12 +0200

    working on project structure

---
+++ src/frontend/frontend/src/lib/data-access/service/backend.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+  readonly url: string = 'http://localhost:4200/api/';
+
+  constructor( private httpClient: HttpClient ) { }
+}
