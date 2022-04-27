105932ba871b8a1ff9b0a28eec58568080d1e4e7
Jakob <jakob.priesner@outlook.de>
2022-04-27 18:15:47 +0200

    added necessary components to app.module.ts

--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {SpecifiedItemComponent} from "../lib/modules/listItem/specifiedItem.component";
-import {ColorCircleComponent} from "../lib/modules/colorCircle/colorCircle.component";
+import {SpecifiedItemComponent} from "../lib/modules/specifiedItem/specifiedItem.component";
+import {ColorCircleComponent} from "../lib/ui/colorCircle/colorCircle.component";
+import {SpecifiedItemListComponent} from "../lib/modules/specifiedItemList/specifiedItemList.component";
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DeleteButtonComponent} from "../lib/ui/deleteButton/deleteButton.component";
 
 @NgModule({
   declarations: [
-    AppComponent, SpecifiedItemComponent, ColorCircleComponent
+    AppComponent, SpecifiedItemComponent, ColorCircleComponent, SpecifiedItemListComponent, DeleteButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
