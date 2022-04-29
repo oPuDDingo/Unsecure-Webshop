71f9ae6c79ba199700d8046a30907867a871046c
Goll, Franziska <franziska.goll@student.fhws.de>
2022-04-29 17:10:07 +0200

    Newsletter implemented

---
+++ src/frontend/frontend/src/lib/pages/Newsletter/newsletter.component.ts
@@ -0,0 +1,11 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'newsletter',
+  templateUrl: './newsletter.component.html',
+  styleUrls: ['./newsletter.component.css']
+})
+export class NewsletterComponent {
+}
+
+//http://localhost:4200/
