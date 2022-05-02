77c1dbd65f7361c591f893f47c91972d8f88e40d
Jakob <jakob.priesner@outlook.de>
2022-05-02 22:07:10 +0200

    minor bugfixes and completion of the component

--- src/frontend/frontend/src/lib/pages/specifiedItemList/specifiedItem/specifiedItem.component.ts
+++ src/frontend/frontend/src/lib/pages/specifiedItemList/specifiedItem/specifiedItem.component.ts
@@ -1,41 +1,39 @@
-import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {SpecifiedItem} from "src/lib/data-access/models";
 
 @Component({
-    selector: 'specified-item',
-    templateUrl: './specifiedItem.component.html',
-    styleUrls: ['./specifiedItem.component.scss']
+  selector: 'specified-item',
+  templateUrl: './specifiedItem.component.html',
+  styleUrls: ['./specifiedItem.component.scss']
 })
 export class SpecifiedItemComponent {
-    @Input() showDeleteButton: boolean = true;
-    @Input() editableQuantity: boolean = true;
-    @Input() specifiedItem: SpecifiedItem;
+  @Input() showDeleteButton: boolean = true;
+  @Input() editableQuantity: boolean = true;
+  // @ts-ignore
+  @Input() specifiedItem: SpecifiedItem;
 
-    @Output() onDeleteEvent: EventEmitter<number> = new EventEmitter<number>();
-    @Output() onQuantityChangeEvent: EventEmitter<number> = new EventEmitter<number>();
+  @Output() onDeleteEvent: EventEmitter<number> = new EventEmitter<number>();
+  @Output() onQuantityChangeEvent: EventEmitter<{ itemId: number, quantity: number }> = new EventEmitter<{ itemId: number, quantity: number }>();
 
-    showTrash: boolean = false;
+  showTrash: boolean = false;
 
-    URL = "";
+  onItemDelete(): void {
+    this.onDeleteEvent.emit(this.specifiedItem.id);
+  }
 
-    constructor() {
-        this.specifiedItem = {id: 1, name: "Iphone 11", storage: 128, articleNumber: 23424342, amount: 1299.99, quantity: 1, color: "blue", picture: "d"};
+  onItemChange(event: any): void {
+    this.onQuantityChangeEvent.emit({itemId: this.specifiedItem.id, quantity: event.target.value});
+  }
 
-    }
+  onMouseEnter(): void {
+    this.showTrash = true;
+  }
 
-    onItemDelete(): void {
+  onMouseLeave(): void {
+    this.showTrash = false;
+  }
 
-    }
-
-    onItemChange(): void {
-
-    }
-
-    onMouseEnter( ): void {
-      this.showTrash = true;
-    }
-
-    onMouseLeave( ): void {
-      this.showTrash = false;
-    }
+  getPictureUrl(): string {
+    return location.origin + '/images/' + this.specifiedItem.picture;
+  }
 }
