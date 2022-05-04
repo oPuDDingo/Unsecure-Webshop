e7d01f4acc1a7646b603f5e9f6d9c658143bf000
Nöth, Tim <tim.noeth@student.fhws.de>
2022-05-04 23:56:49 +0200

    implemented static Impressum

---
+++ src/frontend/frontend/src/lib/ui/Impressum/impressum.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'impressum',
+  templateUrl: './impressum.component.html',
+  styleUrls: ['./impressum.component.scss']
+})
+export class ImpressumComponent {
+
+}
