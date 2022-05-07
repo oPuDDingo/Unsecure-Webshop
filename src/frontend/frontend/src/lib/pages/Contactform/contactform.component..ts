ed516c1fd9b00676034ccd9e6980a21b7f4e6909
Goll, Franziska <franziska.goll@student.fhws.de>
2022-05-07 13:48:32 +0200

    Contactform basic structure

---
+++ src/frontend/frontend/src/lib/pages/Contactform/contactform.component..ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component(
+  {
+    selector: 'contactform',
+    templateUrl: './contactform.component.html',
+    styleUrls: ['./contactform.component.css']
+  }
+)
+
+export class ContactformComponent{
+
+}
