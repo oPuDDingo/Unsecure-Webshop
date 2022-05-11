e23e7d8c326fbdbcd42c0862a9f39510d5df66dc
Jakob <jakob.priesner@outlook.de>
2022-05-11 13:03:03 +0200

    first structure of orderProcess

--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {OrderProcessModule} from "../lib/pages/orderProcess/orderProcess.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OrderProcessModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/frontend/frontend/src/lib/pages/orderProcess/orderProcess.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'order-process',
+  templateUrl: './orderProcess.component.html',
+  styleUrls: ['./orderProcess.component.scss']
+})
+export class OrderProcessComponent {
+
+}
