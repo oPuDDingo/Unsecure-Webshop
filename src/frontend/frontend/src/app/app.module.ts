e9ecd056a31445e0981fd2c5be2d908f6d0bceeb
Goll, Franziska <franziska.goll@student.fhws.de>
2022-05-12 09:49:55 +0200

    Correct app.component.html and app.module.ts

--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {ContactformComponent} from "../lib/pages/Contactform/contactform.component.";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ContactformComponent
   ],
   imports: [
     BrowserModule
