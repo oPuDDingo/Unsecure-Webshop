719e4d3b4757b01bf3ee92a06cbd5b20d4e76a76
Goll, Franziska <franziska.goll@student.fhws.de>
2022-05-12 15:45:57 +0200

    Implented structure of the PageItem

--- src/frontend/frontend/src/lib/pages/ItemList/itemList.component.ts
+++ src/frontend/frontend/src/lib/pages/ItemList/itemList.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'itemList',
   templateUrl: './itemList.component.html',
-  styleUrls: ['./newsletter.component.css']
+  styleUrls: ['./itemList.component.css']
 })
 export class ItemListComponent {
 }
