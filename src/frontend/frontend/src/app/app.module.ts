7d8980f34e8a55a93e90510d7de0b9273521ae2d
Morviller, Valerie <valerie.morviller@student.fhws.de>
2022-05-14 22:24:31 +0200

    create the frontpage Items

--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from "../lib/modules/navbar/navbar.component";
-import { NavbarItemsComponent } from "../lib/modules/navbar.items/navbar.items.component";
-import { FooterComponent } from "../lib/modules/footer/footer.component";
+import {AppComponent} from './app.component';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from "../lib/modules/navbar/navbar.component";
+import {NavbarItemsComponent} from "../lib/modules/navbar.items/navbar.items.component";
+import {FooterComponent} from "../lib/modules/footer/footer.component";
+import {FrontItemGroupComponent} from "../lib/modules/frontItemGroup/frontItemGroup.component";
+import {BoardComponent} from "../lib/modules/board/board.component";
+import {FrontItemComponent} from "../lib/modules/frontItem/frontItem.component";
 
 @NgModule({
-    declarations: [
-      AppComponent,
-      NavbarComponent,
-      NavbarItemsComponent,
-      FooterComponent
-    ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NavbarItemsComponent,
+    FooterComponent,
+    FrontItemGroupComponent,
+    FrontItemComponent,
+    BoardComponent
+  ],
   imports: [
     BrowserModule,
     ModalModule.forRoot(),
@@ -25,4 +31,5 @@ import { FooterComponent } from "../lib/modules/footer/footer.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/frontend/frontend/src/lib/modules/frontItem/frontItem.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'frontItem',
+  templateUrl: './frontItem.component.html',
+  styleUrls: ['./frontItem.component.scss']
+})
+
+export class FrontItemComponent {
+
+}
