feb46c90a721623fe4492db162b6db3ba6e33490
Jakob <jakob.priesner@outlook.de>
2022-05-15 01:21:15 +0200

    implemented the component for the article specifications

--- src/frontend/frontend/src/lib/pages/articleOverview/specifications/specifications.component.ts
+++ src/frontend/frontend/src/lib/pages/articleOverview/specifications/specifications.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Article} from "../../../data-access/models/article";
 
 @Component({
@@ -9,6 +9,15 @@ import {Article} from "../../../data-access/models/article";
 export class SpecificationsComponent {
   // @ts-ignore
   @Input article: Article;
+  @Output() colorChangedEvent: EventEmitter<string> = new EventEmitter<string>();
+  @Output() storageChangeEvent: EventEmitter<number> = new EventEmitter<number>();
 
+  onColorChange(value: string): void {
+    this.colorChangedEvent.emit(value);
+  }
+
+  onStorageChange(value: number): void {
+    this.storageChangeEvent.emit(value);
+  }
 
 }
