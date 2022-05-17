95ad2d3df759be47736f186e6ac61f15329a5072
Stolz Mateos, Alejandro <alejandro.stolzmateos@student.fhws.de>
2022-05-17 06:55:11 +0200

    changed @QueryParam to @CookieParam

--- src/backend/main/java/api/UserService.java
+++ src/backend/main/java/api/UserService.java
@@ -14,7 +14,7 @@ import java.net.URI;
 @Path("user") public class UserService
 {
 	@GET @Path("information") @Produces(MediaType.APPLICATION_JSON) public Response getUser(
-		@QueryParam("sessionID") final String session)
+		@CookieParam("sessionID") final String session)
 	{
 		//get by session id
 		//id = getIdfromDatabase(session)
@@ -22,7 +22,9 @@ import java.net.URI;
 		return Response.ok(User.getRandomUser()).build();
 	}
 
-	@POST @Consumes(MediaType.APPLICATION_JSON) public Response registerUser(@Context UriInfo uriInfo, final User user)
+	@POST @Consumes(MediaType.APPLICATION_JSON) public Response createUser(
+		@Context UriInfo uriInfo,
+		final User user)
 	{
 		// create in database
 		final URI locationURI = uriInfo.getAbsolutePathBuilder().path(Long.toString(user.getId())).build();
@@ -30,47 +32,49 @@ import java.net.URI;
 	}
 
 	@PUT @Consumes(MediaType.APPLICATION_JSON) public Response modifyUser(
-		@QueryParam("sessionID") final String sessionID, final User user)
+		@CookieParam("sessionID") final String sessionID, final User user)
 	{
 		// edit in database
 		return Response.ok(user).build();
 	}
 
-	@DELETE public Response deleteUser(@QueryParam("sessionID") final String sessionID)
+	@DELETE public Response deleteUser(
+		@CookieParam("sessionID") final String sessionID)
 	{
 		//get user from database with sessionID and delete it
 		return Response.noContent().build();
 	}
 
 	@Path("payment") @GET @Produces(MediaType.APPLICATION_JSON) public Response getUserPayment(
-		@QueryParam("sessionID") String sessionID)
+		@CookieParam("sessionID") String sessionID)
 	{
 		// get from database with sessionID
 		return Response.ok(Payment.getRandomPayment()).build();
 	}
 
 	@Path("payment") @PUT @Consumes(MediaType.APPLICATION_JSON) public Response modifyUserPayment(
-		@QueryParam("sessionID") String sessionID, final Payment payment)
+		@CookieParam("sessionID") String sessionID, final Payment payment)
 	{
 		// get from database with sessionID
 		// modify payment in database
 		return Response.ok(payment).build();
 	}
 
-	@Path("payment") @DELETE public Response deleteUserPayment(@QueryParam("sessionID") String sessionID)
+	@Path("payment") @DELETE public Response deleteUserPayment(
+		@CookieParam("sessionID") String sessionID)
 	{
 		// get user with session id and delete related payment info
 		return Response.noContent().build();
 	}
 
 	@GET @Path("{id}") @Produces(MediaType.APPLICATION_JSON) public Response getAddress(
-		@QueryParam("sessionID") String semester, @PathParam("id") final long id)
+		@CookieParam("sessionID") String semester, @PathParam("id") final long id)
 	{
 		return Response.ok(Address.getRandomAddress()).build();
 	}
 
 	@Path("address") @POST @Produces(MediaType.APPLICATION_JSON) public Response createAddress(
-		@QueryParam("sessionID") String sessionID, @Context UriInfo uriInfo, final Address address)
+		@CookieParam("sessionID") String sessionID, @Context UriInfo uriInfo, final Address address)
 	{
 		// post to database with sessionID
 		final URI locationURI = uriInfo.getAbsolutePathBuilder().path(Long.toString(address.getId())).build();
@@ -78,13 +82,15 @@ import java.net.URI;
 	}
 
 	@Path("address") @PUT @Consumes(MediaType.APPLICATION_JSON) public Response modifyAddress(
-		@QueryParam("sessionID") String sessionID, final Address address)
+		@CookieParam("sessionID") String sessionID,
+		final Address address)
 	{
 		// edit in database
 		return Response.ok(address).build();
 	}
 
-	@Path("address/{id}") @DELETE public Response deleteAddress(@QueryParam("sessionID") String sessionID,
+	@Path("address/{id}") @DELETE public Response deleteAddress(
+		@CookieParam("sessionID") String sessionID,
 		@PathParam("id") final int id)
 	{
 		// find user with session ID and corresponding address
@@ -92,14 +98,16 @@ import java.net.URI;
 	}
 
 	@Path("mail") @GET @Produces(MediaType.APPLICATION_JSON) public Response getUserMail(
-		@QueryParam("sessionID") String sessionID)
+		@CookieParam("sessionID") String sessionID)
 	{
 		// get from database with sessionID
 		return Response.ok("email@mail.com").build();
 	}
 
-	@Path("mail") @PUT @Consumes(MediaType.APPLICATION_JSON) public Response createUserMail(
-		@QueryParam("sessionID") String sessionID, @Context UriInfo uriInfo, final String mail)
+	@Path("mail") @POST @Consumes(MediaType.APPLICATION_JSON) public Response createUserMail(
+		@CookieParam("sessionID") String sessionID,
+		@Context UriInfo uriInfo,
+		final String mail)
 	{
 		// get user id from database with sessionID and put new user
 
@@ -107,19 +115,24 @@ import java.net.URI;
 		return Response.created(locationURI).build();
 	}
 
-	@Path("newsletter") @POST public Response turnOnNewsletter()
+	@Path("newsletter") @POST public Response turnOnNewsletter(
+		@CookieParam("sessionID") String sessionID
+	)
 	{
 		// turn on newsletter
 		return Response.ok("signed in for newsletter").build();
 	}
 
-	@Path("newsletter") @DELETE public Response turnOffNewsletter()
+	@Path("newsletter") @DELETE public Response turnOffNewsletter(
+		@CookieParam("sessionID") String sessionID
+	)
 	{
 		// turn off newsletter
 		return Response.ok("cancelled newsletter subscription").build();
 	}
 
-	@Path("password") @PUT public Response modifyPassword(@QueryParam("sessionID") final String sessionID,
+	@Path("password") @PUT public Response modifyPassword(
+		@CookieParam("sessionID") final String sessionID,
 		final String password)
 	{
 		// get user with sessionID
