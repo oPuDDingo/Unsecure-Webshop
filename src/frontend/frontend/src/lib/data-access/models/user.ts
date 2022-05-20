8a3b7384ac389572f8950418ed5c6b835e275776
Nöth, Tim <tim.noeth@student.fhws.de>
2022-05-20 08:46:14 +0200

    added optional to user

--- src/frontend/frontend/src/lib/data-access/models/user.ts
+++ src/frontend/frontend/src/lib/data-access/models/user.ts
@@ -1,8 +1,8 @@
 export interface User {
   id?: number;
   mail: string;
-  firstname: string;
-  lastname: string;
+  firstname?: string;
+  lastname?: string;
   newsletter?: boolean;
   salutation?: string;
   title?: string;
