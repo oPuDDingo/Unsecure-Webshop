a2dbb90e7ac9c174af6a195ddc0c170a03ffd5b0
Jakob <jakob.priesner@outlook.de>
2022-05-22 14:13:15 +0200

    working on order process

---
+++ src/frontend/frontend/src/lib/pages/orderProcess/orderItems/orderItems.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+  selector: 'order-items',
+  templateUrl: './orderItems.component.html',
+  styleUrls: ['./orderItems.component.scss']
+})
+export class OrderItemsComponent {
+
+}
