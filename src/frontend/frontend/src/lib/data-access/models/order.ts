00cd13e8dd4151dd2385ecc3f2f2e9d505b61ed8
Jakob <jakob.priesner@outlook.de>
2022-05-22 14:12:04 +0200

    copied necessary modules

---
+++ src/frontend/frontend/src/lib/data-access/models/order.ts
@@ -0,0 +1,14 @@
+import {SpecifiedItem} from "./specifiedItem";
+import {Coupon} from "./coupon";
+import {Payment} from "./payment";
+import {Address} from "./address";
+
+export interface Order {
+  orderNumber: number;
+  date: string;
+  specifiedItems: SpecifiedItem[];
+  amount?: number;
+  address?: Address[];
+  payment?: Payment[];
+  coupons?: Coupon[];
+}
