d043180f77c8343741bcc423df95d5af11395088
Jakob <jakob.priesner@outlook.de>
2022-05-24 22:55:48 +0200

    added api to endpoint for bugfixes reasons. The frontend proxy cannot applied to "/", only to "/api"

--- src/backend/main/java/Start.java
+++ src/backend/main/java/Start.java
@@ -10,7 +10,7 @@ import java.io.File;
 
 public class Start
 {
-	private static final String CONTEXT_PATH = "/";
+	private static final String CONTEXT_PATH = "/api";
 	private static final String WEB_APP_LOCATION = "src/backend/main/webapp/";
 	private static final String WEB_APP_MOUNT = "/WEB-INF/classes";
 	private static final String WEB_APP_CLASSES = "target/classes";
