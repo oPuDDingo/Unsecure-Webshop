0a377de6bd4d23952ff77f6ba6c68cd34c431dfe
Morviller, Valerie <valerie.morviller@student.fhws.de>
2022-05-31 13:22:19 +0200

    login

---
+++ src/frontend/frontend/src/lib/modules/login/login.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {LoginComponent} from "./login.component";
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+  exports: [LoginComponent]
+})
+
+export class NavbarItemsModule {
+
+}
