2c80ef88fdb44bb4fbdd038bce39c0c28bb2848e
Jakob <jakob.priesner@outlook.de>
2022-05-31 13:36:58 +0200

    bugfixes

---
+++ src/frontend/frontend/src/lib/shared/addresses/addressList.component.ts
@@ -0,0 +1,39 @@
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Address} from "../../data-access/models";
+import {AddressStore} from "../../data-access/service/store/address.store";
+
+@Component({
+  selector: 'addresses',
+  templateUrl: './addressList.component.html',
+  styleUrls: ['./addressList.component.scss']
+})
+export class AddressListComponent implements OnInit {
+  @Input() addresses: Address[] = [];
+  @Input() selectable: boolean = false;
+  @Output() addAddressEvent: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onSelectAddressEvent: EventEmitter<Address> = new EventEmitter<Address>();
+
+  // @ts-ignore
+  selectedAddress: Address;
+
+  constructor(private addressStore: AddressStore) {
+
+  }
+
+  ngOnInit() {
+    if (this.addresses == []) {
+      this.addressStore.loadAllAddresses().subscribe(addresses => {
+        this.addresses = addresses;
+      });
+    }
+  }
+
+  onAddAddress(): void {
+    this.addAddressEvent.emit();
+  }
+
+  onSelectAddress(address: Address): void {
+    this.onSelectAddressEvent.emit(address);
+  }
+
+}
