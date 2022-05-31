0b6b04ca9a3322d46c863e9191fa8f11f8389899
Nöth, Tim <tim.noeth@student.fhws.de>
2022-05-31 14:32:26 +0200

    deleted dummyData

--- src/frontend/frontend/src/lib/pages/wishList/wishlist.component.ts
+++ src/frontend/frontend/src/lib/pages/wishList/wishlist.component.ts
@@ -12,56 +12,6 @@ export class WishlistComponent implements OnInit {
   showText: boolean = false;
 
   constructor(private wishlistStore: WishlistStore) {
-    // this.itemList = [{
-    //   id: 1,
-    //   articleNumber: 23424342,
-    //   name: "Iphone 11",
-    //   quantity: 1,
-    //   gbSize: 128,
-    //   color: "blue",
-    //   amount: 1299.99,
-    //   pictureId: 1
-    // },
-    //   {
-    //     id: 2,
-    //     articleNumber: 41561651,
-    //     name: "Iphone 10",
-    //     quantity: 2,
-    //     gbSize: 128,
-    //     color: "white",
-    //     amount: 899.99,
-    //     pictureId: 2
-    //   },
-    //   {
-    //     id: 3,
-    //     articleNumber: 724345,
-    //     name: "Iphone 13",
-    //     gbSize: 256,
-    //     quantity: 1,
-    //     amount: 1499.99,
-    //     color: "black",
-    //     pictureId: 3
-    //   },
-    //   {
-    //     id: 4,
-    //     articleNumber: 9873198,
-    //     name: "Samsung S22",
-    //     gbSize: 128,
-    //     quantity: 4,
-    //     amount: 800.00,
-    //     color: "red",
-    //     pictureId: 4
-    //   },
-    //   {
-    //     id: 5,
-    //     name: "Iphone 7",
-    //     gbSize: 512,
-    //     articleNumber: 1757356,
-    //     amount: 599.00,
-    //     quantity: 1,
-    //     color: "green",
-    //     pictureId: 5
-    //   }];
   }
 
   onWishListDelete(): void {
@@ -73,7 +23,7 @@ export class WishlistComponent implements OnInit {
   }
 
   onItemChange(event: any): void {
-    this.wishlistStore.updateItem(event.itemId, event.quantity)
+    this.wishlistStore.updateItem(event.itemId, event.quantity);
   }
 
   ngOnInit(): void {
