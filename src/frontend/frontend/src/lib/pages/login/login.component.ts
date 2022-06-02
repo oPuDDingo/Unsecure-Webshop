4411464ff333d35d4f1eee269677780484ca9813
Jakob <jakob.priesner@outlook.de>
2022-06-02 13:01:25 +0200

    working on an alert massage for the login page

--- src/frontend/frontend/src/lib/pages/login/login.component.ts
+++ src/frontend/frontend/src/lib/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
-import {BackendService} from "../../data-access/service/backend.service";
 import {Router} from "@angular/router";
+import {AuthenticationService} from "../../data-access/service/authentication.service";
 
 @Component({
   selector: 'login',
@@ -12,10 +12,12 @@ export class LoginComponent {
   mail: string = "";
   password: string = "";
 
-  constructor(private backendService: BackendService, private router: Router) {
+  invalidData: boolean = false;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
   onLogin() {
-    this.backendService.login(this.mail, this.password).subscribe(() => this.router.navigateByUrl('/user'));
+    this.authenticationService.login(this.mail, this.password).subscribe(() => this.router.navigateByUrl('/user'), (error) => this.invalidData = true);
   }
 }
