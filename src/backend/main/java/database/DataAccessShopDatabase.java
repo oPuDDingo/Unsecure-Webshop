519eca57b15c38345b3ac7aceb97f51e47b55205
Hepp, Levin <levin.hepp@student.fhws.de>
2022-06-02 13:11:19 +0200

    fixed Bugs

--- src/backend/main/java/database/DataAccessShopDatabase.java
+++ src/backend/main/java/database/DataAccessShopDatabase.java
@@ -128,7 +128,7 @@ public class DataAccessShopDatabase {
             stmt = con.createStatement();
             String sql = "UPDATE address " +
                     " SET(street_house_number='" + address.getAddress() + "', postcode= '" + address.getZipCode() + "', address=supplement='" + address.getAddress2() + "', city='" + address.getCity() + "', country='" + address.getCountry()
-                    + "', name='" + address.getName() + "', delivery_instruction='" + address.getDeliveryInstructions() + "' WHERE id=" + address.getId() + ";";
+                    + "', name='" + address.getName() + "', delivery_instruction='" + address.getDeliveryInstructions() + "' WHERE id=" + address.getId() +"AND"+"user_id="+userId+";";
             stmt.execute(sql);
             addressRet = this.getAddress(address.getId());
             stmt.close();
