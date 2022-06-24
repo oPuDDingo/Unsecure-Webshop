2c6878a01a53a047918c8bc2ffa2b7d97b45c03b
Nöth, Tim <tim.noeth@student.fhws.de>
2022-06-24 20:27:14 +0200

    added adminLogin errormessage

--- src/frontend/frontend/src/lib/pages/adminLogin/adminLogin.module.ts
+++ src/frontend/frontend/src/lib/pages/adminLogin/adminLogin.module.ts
@@ -3,6 +3,7 @@ import {AdminLoginComponent} from "./adminLogin.component";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
+import {AlertModule} from "ngx-bootstrap/alert";
 
 const routes: Routes = [
   {path: '', component: AdminLoginComponent}
@@ -10,11 +11,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AdminLoginComponent],
-  imports: [
-    FormsModule,
-    CommonModule,
-    RouterModule.forChild(routes)
-  ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        RouterModule.forChild(routes),
+        AlertModule
+    ],
   providers: [],
   bootstrap: [],
   exports: [AdminLoginComponent]
