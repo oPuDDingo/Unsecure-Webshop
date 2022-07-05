85b4e3cc3e7e4d209ca768610b83078490c7a772
Jakob <jakob.priesner@outlook.de>
2022-07-05 16:36:53 +0200

    changed title of website

--- src/frontend/frontend/src/app/app.component.ts
+++ src/frontend/frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Bidermanns Shop';
 
   constructor(private route: ActivatedRoute) {
   }
