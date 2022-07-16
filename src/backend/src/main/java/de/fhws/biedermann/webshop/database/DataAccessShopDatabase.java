9120f91bde486b93f7e466944d8bcafa793a70f0
Hepp, Levin <levin.hepp@student.fhws.de>
2022-07-16 16:10:44 +0200

    little bug fix

--- src/backend/src/main/java/de/fhws/biedermann/webshop/database/DataAccessShopDatabase.java
+++ src/backend/src/main/java/de/fhws/biedermann/webshop/database/DataAccessShopDatabase.java
@@ -889,7 +889,7 @@ public class DataAccessShopDatabase {
             for (User u : DatabaseQueries.bidermannUser) {
                 stmt.execute("INSERT INTO user(e_mail, firstname, lastname, password, newsletter, salutation, title, profile_picture, real_user, description) " +
                         "VALUES('" + u.getMail() + "', '" + u.getFirstName() + "', '" + u.getLastName() + "', '" + this.encryptPasswordRealUser(u.getPassword()) + "', " + u.isNewsletter() + ",'" +
-                        u.getSalutation() + "', '" + u.getTitle() + "', '" + u.getProfilePicture() + "', " + false + ",'" + u.getDescription() + "');");
+                        u.getSalutation() + "', '" + u.getTitle() + "', '" + u.getProfilePicture() + "', " + true + ",'" + u.getDescription() + "');");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -994,10 +994,5 @@ public class DataAccessShopDatabase {
             e.printStackTrace();
         }
         return 0;
-    }
-
-    public static void main(String[] args){
-        DataAccessShopDatabase d = new DataAccessShopDatabase();
-        System.out.println(d.encryptPasswordRealUser("Xb935uFoLaC!82?Z"));
     }
 }
