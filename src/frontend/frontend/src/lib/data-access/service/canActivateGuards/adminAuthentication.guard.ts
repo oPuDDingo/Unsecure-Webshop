1dc293f877eef579c956b1b10647ac9007f3d3c8
Jakob <jakob.priesner@outlook.de>
2022-07-18 23:08:48 +0200

    fixed missing check in canActivateGuard for adminPage

--- src/frontend/frontend/src/lib/data-access/service/canActivateGuards/adminAuthentication.guard.ts
+++ src/frontend/frontend/src/lib/data-access/service/canActivateGuards/adminAuthentication.guard.ts
@@ -2,16 +2,19 @@ import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Observable} from "rxjs";
 import {CookieService} from "ngx-cookie-service";
+import {AuthenticationService} from "../authentication.service";
+import {UserTypes} from "../../enums/userTypes";
 
 @Injectable({providedIn: 'root'})
 export class AdminAuthenticationGuard implements CanActivate {
 
-  constructor(private router: Router, private cookieService: CookieService) {
+  constructor(private router: Router, private cookieService: CookieService, private authenticationService: AuthenticationService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.cookieService.check('sessionKey')) {
+              state: RouterStateSnapshot,
+              ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.cookieService.check('sessionKey') && this.authenticationService.userType == UserTypes.Admin) {
       return true;
     }
 
