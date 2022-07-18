166700df0874e57e429e6a56343730850972305b
Nöth, Tim <tim.noeth@student.fhws.de>
2022-07-18 19:30:15 +0200

    implemented priceOrderBug

--- src/frontend/frontend/src/lib/pages/shoppingCart/shoppingCart.component.ts
+++ src/frontend/frontend/src/lib/pages/shoppingCart/shoppingCart.component.ts
@@ -1,6 +1,8 @@
 import {Component} from "@angular/core";
 import {SpecifiedItem} from "src/lib/data-access/models";
 import {ShoppingCartStore} from "../../data-access/service/store/shoppingCart.store";
+import {BackendService} from "../../data-access/service/backend.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'shopping-cart',
@@ -8,13 +10,13 @@ import {ShoppingCartStore} from "../../data-access/service/store/shoppingCart.st
   styleUrls: ['./shoppingCart.component.scss']
 })
 export class ShoppingCartComponent {
-  // @ts-ignore
-  itemList: SpecifiedItem[];
+
+  itemList: SpecifiedItem[] | undefined;
 
   amount: number = 0;
   quantity: number = 0;
 
-  constructor(private shoppingCartStore: ShoppingCartStore) {
+  constructor(private shoppingCartStore: ShoppingCartStore, private backendService: BackendService, private router: Router) {
   }
 
   onItemDelete(itemId: number): void {
@@ -46,5 +48,16 @@ export class ShoppingCartComponent {
         this.quantity += element.quantity;
       });
     }
+  }
+
+  onNavigateToOrderProcess(): void {
+    this.backendService.getLevel().subscribe( levelNumber => {
+      if (levelNumber == 1)
+      {
+        this.router.navigate!(['/checkout'], {queryParams: {amount: this.amount}});
+      } else {
+         this.router.navigateByUrl!( "/checkout" );
+      }
+    } );
   }
 }
--- src/frontend/frontend/src/lib/pages/shoppingCart/shoppingCart.module.ts
+++ src/frontend/frontend/src/lib/pages/shoppingCart/shoppingCart.module.ts
@@ -4,6 +4,7 @@ import {AccordionModule} from 'ngx-bootstrap/accordion';
 import {ShoppingCartComponent} from "./shoppingCart.component";
 import {SpecifiedItemListModule} from "../../shared/specifiedItemList/specifiedItemList.module";
 import {RouterModule, Routes} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 
 const routes: Routes = [
@@ -14,11 +15,12 @@ const routes: Routes = [
   declarations: [
     ShoppingCartComponent
   ],
-  imports: [
-    AccordionModule.forRoot(),
-    SpecifiedItemListModule,
-    RouterModule.forChild(routes)
-  ],
+    imports: [
+        AccordionModule.forRoot(),
+        SpecifiedItemListModule,
+        RouterModule.forChild(routes),
+        CommonModule
+    ],
   providers: [],
   bootstrap: [],
   exports: [ShoppingCartComponent]
